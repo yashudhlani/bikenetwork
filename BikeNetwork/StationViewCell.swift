6cd85caf0b317b4dec7dac3567490361ef7ebb0d
Yash Udhlani <udhlaniyash@gmail.com>
2022-07-29 06:04:04 +0800

    Initial Commit

---
+++ BikeNetwork/StationViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StationViewCell.swift
+//  BikeNetwork
+//
+//  Created by user225501 on 7/29/22.
+//
+
+import UIKit
+
+class StationViewCell: UITableViewCell {
+
+    @IBOutlet weak var stationName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
